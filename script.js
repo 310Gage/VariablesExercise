67ec8ee1db911f15dcfa3bdee7c5d1328173d953
Gage Hall <Gageh310@gmail.com>
2022-09-28 10:45:35 -0400

    VariablesExercise comeplete

---
+++ script.js
@@ -0,0 +1,30 @@
+const abc = 123;
+
+let num = 20;
+
+num = 15;
+
+num += 7;
+
+num--;
+
+num = 'This is not a number';
+
+console.log(num);
+
+let blank;
+
+console.log(blank);
+
+blank = null;
+
+console.log(blank);
+
+const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+
+console.log(quarter, dime, nickel, penny);
+
+let letters = 'qwertyuiop'
+
+console.log(letters.length)
+console.log(letters[9]);
